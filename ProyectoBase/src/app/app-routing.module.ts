a3f398edfd6cb01feddc1ef6b2d08afa4491ed5b
Chelsea Olivares <cm.olivares@hotmail.com>
2019-09-18 01:30:44 -0500

    Login y Registro

--- ProyectoBase/src/app/app-routing.module.ts
+++ ProyectoBase/src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },  { path: 'registrar', loadChildren: './auth/registrar/registrar.module#RegistrarPageModule' },
+  },
+  { path: 'registrar', loadChildren: './auth/registrar/registrar.module#RegistrarPageModule' },
   { path: 'login', loadChildren: './auth/login/login.module#LoginPageModule' }
 
 ];
